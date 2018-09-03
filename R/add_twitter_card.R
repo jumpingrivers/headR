6c268a0bc680a72d7e35e642597b301bf9d4a483
theoroe3 <theo@jumpingrivers.com>
2018-09-03 12:58:08 +0100

    description, image and user are now optional

--- R/add_twitter_card.R
+++ R/add_twitter_card.R
@@ -1,36 +1,61 @@
 #' @importFrom glue glue
 #' @title Create twitter summary cards
 #'
-#' @description An easy way to create twitter summary cards for your blogdown post
+#' @description An easy way to create twitter summary cards for your blogdown post.
+#'     For more details on the meta tags see https://developer.twitter.com/en/docs/tweets/optimize-with-cards/overview/summary.html
 #'
-#' @param card_type must be "summary" or "summary_large_image"
-#' @param user twitter user id for the card to be linked to
-#' @param content_title title for card content, max 70 characters
-#' @param content_description description for card content, max 200 characters
-#' @param content_image link for the content image
-#' @param file name to put in the header of Rmd doc
+#' @param card_type Twitter card type. Must be "summary" or "summary_large_image".
+#' @param user Twitter user id for the card to be linked to.
+#' @param content_title Title for card content, max 70 characters. All twitter cards require this.
+#' @param content_description Description for card content, max 200 characters.
+#' @param content_image Link for the content image.
+#' @param file File name to put in the header of Rmd doc, must be .HTML.
 #'
 #' @export
 #'
 #' @examples
-#' add_twitter_card()
+#' add_twitter_card(content_title = "Hello", file = "hello.HTML")
 add_twitter_card = function(card_type = "summary",
-                            user = "@jumping_uk",
-                            content_title = "Content title",
-                            content_description = "Content description.",
-                            content_image = "https://www.rstudio.com/wp-content/uploads/2014/06/RStudio-Ball.png",
-                            file = "twitter-card.html") {
-  if(!(card_type %in% c("summary", "summary_large_image"))) {
-    stop("Card type should be either summary or summary_large_image", call. = FALSE)
+                            user = NULL,
+                            content_title = NULL,
+                            content_description = NULL,
+                            content_image = NULL,
+                            file = NULL) {
+  if (is.null(content_title)) {
+    stop(
+      "Cards must have a content title as specified by twitter, see
+      https://developer.twitter.com/en/docs/tweets/optimize-with-cards/overview/summary.html
+      for details",
+      call. = FALSE
+    )
+  } else{
+    if (nchar(content_title) > 70) {
+      stop("Content title must be <= 70 characters, please amend",
+           call. = FALSE)
+    }
   }
-  if(nchar(content_title) > 70 || nchar(content_description) > 200){
-    stop("Too many characters for title or description, please amend", call. = FALSE)
+  if (is.null(file)) {
+    stop("You must specify a file name to be used in your Rmd header")
   }
-  twitter_card = glue('<meta name="twitter:card" content="{card_type}">
-              <meta name="twitter:site" content="{user}">
-              <meta name="twitter:title" content="{content_title}">
-              <meta name="twitter:description" content="{content_description}">
-              <meta name="twitter:image" content="{content_image}">')
+  if (!(card_type %in% c("summary", "summary_large_image"))) {
+    stop("Card type should be either summary or summary_large_image",
+         call. = FALSE)
+  }
+  if (!is.null(content_description)) {
+    if (nchar(content_description) > 200) {
+      stop("Content description must be <= 200 characters, please amend",
+           call. = FALSE)
+    }
+  }
+  twitter_card = c(
+    card = card_type,
+    site = user,
+    title = content_title,
+    description = content_description,
+    image = content_image
+  )
+
+  twitter_card = glue('<meta name="twitter:{names(twitter_card)}" content="{twitter_card}">')
 
   writeLines(text = twitter_card, con = file)
 }

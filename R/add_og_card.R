851ce75a488fa5d74cbc3627f647888d8b8a5483
theoroe3 <theo@jumpingrivers.com>
2018-10-11 16:37:02 +0100

    added checker functions and open graph function

---
+++ R/add_og_card.R
@@ -0,0 +1,96 @@
+#' @title Checking function arguments
+#'
+#' @param card_type Required. Open graph card type. See http://ogp.me/#types for list
+#' @param title Required. Title for card content.
+#' @param image Required. Link for the content image.
+#' @param url Required. Canonical URl to be used as permanent ID in the graph.
+#' @param file Required. File name to put in the header of Rmd doc, must be .html.
+check_og_arguments = function(card_type,
+                                   title,
+                                   image,
+                                   url,
+                                   file){
+  if(any(missing(title),
+         missing(image), missing(url))){
+    stop("Open Graph requires meta tags for type, title, image and url.
+          A file name must also be supplied.")
+  }
+
+  if (!(card_type %in% c("website", "article", "book"))) {
+    stop("headR currently only supports website, article and book OG card types.",
+         call. = FALSE)
+  }
+
+  if (nchar(file) < 1 ||
+      substr(file, nchar(file) - 4, nchar(file)) != ".html") {
+    stop("You must specify a valid file name to be used in your Rmd header")
+  }
+}
+
+#' @importFrom glue glue
+#' @title Create open graph meta tags
+#'
+#' @description An easy way to create open graph meta tags for your blogdown post.
+#'     For more details on the meta tags see http://ogp.me/
+#'
+#' @param card_type Required. Open graph card type. See http://ogp.me/#types for list
+#' @param title Required. Title for card content.
+#' @param image Required. Link for the content image.
+#' @param url Required. Canonical URl to be used as permanent ID in the graph.
+#' @param image_alt Alternate text to display if image fails.
+#' @param description Description for card content. One to two sentences.
+#' @param site_name If your object is part of a larger web site, the name which should be displayed for the overall site.
+#' @param video URL to a video file that complements the object
+#' @param audio URL to an audio file that complements the object
+#' @param author Authors of the content if content type is "article" or "book"
+#' @param file Required. File name to put in the header of Rmd doc, must be .html.
+#'
+#' @export
+#'
+#' @examples
+#' add_og_card(title = "HeadR is great!",
+#'        image = "https://www.rstudio.com/wp-content/uploads/2014/06/RStudio-Ball.png",
+#'        url = "www.jumpingrivers.com",
+#'        file = "hello.html")
+add_og_card = function(card_type = c("website", "article", "book"),
+                            title,
+                            image,
+                            url,
+                            description = NULL,
+                            image_alt = NULL,
+                            site_name = NULL,
+                            video = NULL,
+                            audio = NULL,
+                            author = NULL,
+                            file = "og_card.html") {
+  card_type = card_type[1]
+
+  check_og_arguments(card_type,
+                          title,
+                          image,
+                          url,
+                          file)
+
+  og_card = c(
+    card = card_type,
+    title = title,
+    image = image,
+    url = url,
+    `image:alt` = image_alt,
+    description = description,
+    site_name = site_name,
+    video = video,
+    audio = audio
+  )
+
+  og_card = glue('<meta property="og:{names(og_card)}" content="{og_card}">')
+
+  if(!missing(author)){
+    if(author %in% c("article", "book")){
+       og_card = glue(og_card,
+                     '<meta property="{card_type}:author content="{author}"')
+    }
+  }
+  writeLines(text = og_card, con = file)
+}
+
--- R/add_twitter_card.R
+++ R/add_twitter_card.R
@@ -1,58 +1,82 @@
+#' @title checking function arguments
+#' @param card_type Required. Twitter card type. Must be "summary" or "summary_large_image".
+#' @param title Required. Title for card content. Max 70 characters.
+#' @param description Description for card content, max 200 characters.
+#' @param file Required. File name to put in the header of Rmd doc, must be .html.
+#'
+check_twitter_arguments = function(card_type,
+                                   title,
+                                   description,
+                                   file) {
+  if (!(card_type %in% c("summary", "summary_large_image"))) {
+    stop("Card type should be either summary or summary_large_image",
+         call. = FALSE)
+  }
+
+  if (missing(title)) {
+    stop(
+      "Cards must have a content title as specified by twitter,
+      see description for details",
+      call. = FALSE
+    )
+  }
+  if (nchar(title) > 70) {
+    stop("Content title must be <= 70 characters,
+         please amend",
+         call. = FALSE)
+  }
+  if (!is.null(description) && nchar(description) > 200) {
+    stop("Content description must be <= 200 characters,
+         please amend",
+         call. = FALSE)
+  }
+
+  if (nchar(file) < 1 ||
+      substr(file, nchar(file) + 1 - 5, nchar(file)) != ".html") {
+    stop("You must specify a valid file name to be used in your Rmd header")
+  }
+}
+
+
 #' @importFrom glue glue
 #' @title Create twitter summary cards
 #'
 #' @description An easy way to create twitter summary cards for your blogdown post.
 #'     For more details on the meta tags see https://developer.twitter.com/en/docs/tweets/optimize-with-cards/overview/summary.html
 #'
-#' @param card_type Twitter card type. Must be "summary" or "summary_large_image".
+#' @param card_type Required. Twitter card type. Must be "summary" or "summary_large_image".
 #' @param user Twitter user id for the card to be linked to.
-#' @param content_title Title for card content, max 70 characters. All twitter cards require this.
-#' @param content_description Description for card content, max 200 characters.
-#' @param content_image Link for the content image.
-#' @param file File name to put in the header of Rmd doc, must be .HTML. Defaults to twitter_card.HTML
+#' @param title Required. Title for card content. Max 70 characters.
+#' @param description Description for card content, max 200 characters.
+#' @param image Link for the content image.
+#' @param image_alt Alternate text incase image does not load.
+#' @param file Required. File name to put in the header of Rmd doc, must be .html.
 #'
 #' @export
 #'
 #' @examples
-#' add_twitter_card(content_title = "Hello", file = "hello.HTML")
-add_twitter_card = function(card_type = "summary",
+#' add_twitter_card(title = "HeadR is great!", file = "hello.html")
+add_twitter_card = function(card_type = c("summary", "summary_large_image"),
+                            title,
+                            description = NULL,
                             user = NULL,
-                            content_title = NULL,
-                            content_description = NULL,
-                            content_image = NULL,
-                            file = "twitter_card.HTML") {
-  if (is.null(content_title)) {
-    stop(
-      "Cards must have a content title as specified by twitter, see
-      https://developer.twitter.com/en/docs/tweets/optimize-with-cards/overview/summary.html
-      for details",
-      call. = FALSE
-    )
-  } else{
-    if (nchar(content_title) > 70) {
-      stop("Content title must be <= 70 characters, please amend",
-           call. = FALSE)
-    }
-  }
-  if (nchar(file) < 1) {
-    stop("You must specify a valid file name to be used in your Rmd header")
-  }
-  if (!(card_type %in% c("summary", "summary_large_image"))) {
-    stop("Card type should be either summary or summary_large_image",
-         call. = FALSE)
-  }
-  if (!is.null(content_description)) {
-    if (nchar(content_description) > 200) {
-      stop("Content description must be <= 200 characters, please amend",
-           call. = FALSE)
-    }
-  }
+                            image = NULL,
+                            image_alt = NULL,
+                            file = "twitter_card.html") {
+  card_type = card_type[1]
+
+  check_twitter_arguments(card_type,
+                          title,
+                          description,
+                          file)
+
   twitter_card = c(
     card = card_type,
     site = user,
-    title = content_title,
-    description = content_description,
-    image = content_image
+    title = title,
+    description = description,
+    image = image,
+    `image:alt` = image_alt
   )
 
   twitter_card = glue('<meta name="twitter:{names(twitter_card)}" content="{twitter_card}">')
